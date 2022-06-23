a88eda0f68844879b3feb1813c4110e2c768868f
Maciej Nawrot <macjeowsky315@gmail.com>
2022-06-23 21:10:24 +0200

    Revert "Revert "proper resizing of image page""
    This reverts commit ea0ce0fd43c452ab46215f83582db02245762e01.

--- lib/picture_tile.dart
+++ lib/picture_tile.dart
@@ -73,7 +73,7 @@ class _PictureTileState extends State<PictureTile> {
                   )
                 : Column(children: [
                     GestureDetector(
-                      onTap: () => _goToDetails(widget.picture),
+                      onTap: () => debugPrint("picture clicked"),
                       child: widget.picture.image!,
                     ),
                     Flex(
